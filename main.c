b47dca80649effd7402d62e4a22fcba2317c65da
aningsk <aningsk@foxmail.com>
2026-01-21 13:54:28 +0800

    Remove debug of print mpidr_el1

--- main.c
+++ main.c
@@ -8,14 +8,8 @@ __attribute__((aligned(SZ_4K))) char sp_stack[SZ_4K * NCPU] = { 0 };
 
 int main(void)
 {
-	unsigned long long mpidr = 0;
-
 	pl011_init();
-
 	printf("Hello, world!\n");
-
-	asm volatile("mrs %0, mpidr_el1" : "=r" (mpidr));
-	printf("> mpidr_el1 = 0x%x\n", mpidr);
 
 	return 0;
 }
