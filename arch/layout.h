4eac00d70b34270d73d42075cdfb42ec1f9a36a5
aningsk <aningsk@foxmail.com>
2026-01-28 14:18:32 +0800

    Change hypervisor memory size to 128MB

--- arch/layout.h
+++ arch/layout.h
@@ -5,7 +5,7 @@
 #define HIMAGE_VADDR    0x40200000
 
 #define PHYBASE         0x40000000
-#define PHYSIZE         (512 * 1024 * 1024)
+#define PHYSIZE         (128 * 1024 * 1024)
 #define PHYEND          (PHYBASE + PHYSIZE)
 
 #endif /* _LAYOUT_H */
