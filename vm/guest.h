45d3af925a10c8f0daaccbb6391b645cf919851e
aningsk <aningsk@foxmail.com>
2026-03-25 09:52:45 +0800

    Code style on vm header files

--- vm/guest.h
+++ vm/guest.h
@@ -4,9 +4,9 @@
 #include "types.h"
 
 typedef struct guest {
-    char *guest_name;
-    u64   start_addr;
-    u64   image_size;
+	char *guest_name;
+	u64 start_addr;
+	u64 image_size;
 } guest_t;
 
 #endif /* _GUEST_H */
--- vm/vm.h
+++ vm/vm.h
@@ -7,20 +7,20 @@
 #include "vcpu.h"
 
 typedef struct vm_config {
-    guest_t *guest_image;
-    guest_t *guest_dtb;
-    guest_t *guest_initrd;
-    u64      entry_addr;
-    u64      ram_size;
-    int      ncpu;
+	guest_t *guest_image;
+	guest_t *guest_dtb;
+	guest_t *guest_initrd;
+	u64 entry_addr;
+	u64 ram_size;
+	int ncpu;
 } vm_config_t;
 
 typedef struct vm {
-    char       name[32];
-    int        nvcpu;
-    u64       *vttbr;
-    spinlock_t vm_lock;
-    struct vcpu *vcpus[NCPU];
+	char name[32];
+	int nvcpu;
+	u64 *vttbr;
+	spinlock_t vm_lock;
+	struct vcpu *vcpus[NCPU];
 } vm_t;
 
 void create_guest_vm(vm_config_t *vm_config);
